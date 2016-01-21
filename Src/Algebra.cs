84b8e52c284514c17efcb3df099725531a431236
Thierry Matthey <matthey@gmail.com>
2016-01-22 00:43:35 +0100

    Added polynomial with derivative and integral

---
+++ Src/Algebra.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math.Algebra
+{
+    public class Polynomial
+    {
+        private readonly IList<double> _p;
+        private readonly IList<double> _dp;
+        private readonly IList<double> _P;
+
+        public Polynomial(IList<double> coefficients)
+        {
+            // Polynomial
+            _p = new List<double>(coefficients);
+
+            if (_p.Count == 0)
+                _p.Add(0.0);
+
+            // Derivative
+            _dp = new List<double>();
+            for (var i = 1; i < _p.Count; i++)
+            {
+                _dp.Add(_p[i] * i);
+            }
+
+            // Integral
+            _P = new List<double>();
+            _P.Add(0.0);
+            for (var i = 0; i < _p.Count; i++)
+            {
+                _P.Add(_p[i] / (i+1));
+            }
+        }
+
+        public IList<double> p() { return _p; }
+        public IList<double> dp() { return _dp; }
+        public IList<double> P() { return _P; }
+
+        public double p(double x) { return eval(x, _p); }
+        public double dp(double x) { return eval(x, _dp); }
+        public double P(double x) { return eval(x, _P); }
+
+        private double eval(double x, IList<double> p)
+        {
+            double y = 0;
+            double u = 1;
+            foreach(var c in p)
+            {
+                y += u * c;
+                u *= x;
+            }
+            return y;
+        }
+    }
+}
---
+++ Tests/AlgebraTests.cs
@@ -0,0 +1,102 @@
+﻿using Math.Algebra;
+using NUnit.Framework;
+using Shouldly;
+using System.Collections.Generic;
+
+namespace Math.Test
+{
+    [TestFixture]
+    public class AlgebraTests
+    {
+        [Test]
+        public void Polynomial_WithEmptyCoefficients_ReturnsZeroFunction()
+        {
+            var p = new Polynomial(new List<double>() { });
+            p.dp().Count.ShouldBe(0);
+            p.p().Count.ShouldBe(1);
+            p.p()[0].ShouldBe(0.0);
+            p.P().Count.ShouldBe(2);
+            p.P()[0].ShouldBe(0.0);
+            p.P()[1].ShouldBe(0.0);
+        }
+
+        [TestCase(1.1)]
+        [TestCase(-1.1)]
+        [TestCase(0)]
+        public void Polynomial_WithConstant_ReturnsConstantFunction(double c)
+        {
+            var p = new Polynomial(new List<double>() { c });
+            p.dp().Count.ShouldBe(0);
+            p.p().Count.ShouldBe(1);
+            p.p()[0].ShouldBe(c);
+            p.P().Count.ShouldBe(2);
+            p.P()[0].ShouldBe(0.0);
+            p.P()[1].ShouldBe(c);
+        }
+
+        [TestCase(1.0, 2.0)]
+        [TestCase(-1.0, 2.0)]
+        [TestCase(1.0, -2.0)]
+        [TestCase(-1.0, -2.0)]
+        [TestCase(-1.0, -1.0)]
+        [TestCase(0.0, 0.0)]
+        [TestCase(1.0, 1.0)]
+        public void Polynomial_pWithRoot_ReturnsZero(double x0, double x1)
+        {
+            double a2, a1, a0;
+            CreateEq(x0, x1, out a2, out a1, out a0);
+            var p = new Polynomial(new List<double>() {a0, a1, a2});
+
+            p.p(x0).ShouldBe(0.0);
+            p.p(x1).ShouldBe(0.0);
+
+
+        }
+
+        [TestCase(1.0, 2.0)]
+        [TestCase(-1.0, 2.0)]
+        [TestCase(1.0, -2.0)]
+        [TestCase(-1.0, -2.0)]
+        [TestCase(-1.0, -1.0)]
+        [TestCase(0.0, 0.0)]
+        [TestCase(1.0, 1.0)]
+        public void Polynomial_IntegrateDerivative_ReturnsPolynomial(double x0, double x1)
+        {
+            double a2, a1, a0;
+            CreateEq(x0, x1, out a2, out a1, out a0);
+            var p = new Polynomial(new List<double>() { a0, a1, a2 });
+            var res = new Polynomial(p.dp()).P();
+            res.Count.ShouldBe(p.p().Count);
+            // Constant not preserved since first applying derivative
+            for (var i = 1; i < res.Count; ++i) 
+                res[i].ShouldBe(p.p()[i]);
+
+        }
+
+        [TestCase(1.0, 2.0)]
+        [TestCase(-1.0, 2.0)]
+        [TestCase(1.0, -2.0)]
+        [TestCase(-1.0, -2.0)]
+        [TestCase(-1.0, -1.0)]
+        [TestCase(0.0, 0.0)]
+        [TestCase(1.0, 1.0)]
+        public void Polynomial_DerivativeIntegrate_ReturnsPolynomial(double x0, double x1)
+        {
+            double a2, a1, a0;
+            CreateEq(x0, x1, out a2, out a1, out a0);
+            var p = new Polynomial(new List<double>() { a0, a1, a2 });
+            var res = new Polynomial(p.P()).dp();
+            res.Count.ShouldBe(p.p().Count);
+            for (var i = 0; i < res.Count; ++i)
+                res[i].ShouldBe(p.p()[i]);
+
+        }
+
+        private void CreateEq(double x0, double x1, out double a, out double b, out double c)
+        {
+            a = 1.0;
+            b = -(x0 + x1);
+            c = x0 * x1;
+        }
+    }
+}
