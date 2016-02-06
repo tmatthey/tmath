9b23e6b334dba55e71875807cae29112d82c2fe3
Thierry Matthey <matthey@gmail.com>
2016-02-07 00:18:43 +0100

    Added conversion deg <-> rad Added Haversine to calculate distance between to points on earth.

---
+++ Src/Geodesy.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+namespace Math
+{
+    public static class Geodesy
+    {
+        // Latitude and longitude are in degrees, output in meter
+        public static double Haversine(double lat1, double long1, double lat2, double long2)
+        {
+            lat1 = Conversion.DegToRad(lat1);
+            long1 = Conversion.DegToRad(long1);
+            lat2 = Conversion.DegToRad(lat2);
+            long2 = Conversion.DegToRad(long2);
+
+            var dlong = long2 - long1;
+            var dlat = lat2 - lat1;
+            var a = System.Math.Pow(System.Math.Sin(dlat / 2.0), 2) + System.Math.Cos(lat1) * System.Math.Cos(lat2) * System.Math.Pow(System.Math.Sin(dlong / 2.0), 2);
+            var c = 2 * System.Math.Atan2(System.Math.Sqrt(a), System.Math.Sqrt(1 - a));
+            var d = 6367000.0 * c;
+
+            return d;
+        }
+    }
+}
---
+++ Tests/ConversionTests.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+using NUnit.Framework;
+using Shouldly;
+
+namespace Math.Tests
+{
+    [TestFixture]
+    public class ConversionTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(90, 0.5 * System.Math.PI)]
+        [TestCase(180, System.Math.PI)]
+        [TestCase(360, 2.0 * System.Math.PI)]
+        [TestCase(-90, -0.5 * System.Math.PI)]
+        [TestCase(-180, -System.Math.PI)]
+        [TestCase(-360, -2.0 * System.Math.PI)]
+        public void DegToRad(double d, double r)
+        {
+            Conversion.DegToRad(d).ShouldBe(r);
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(90, 0.5 * System.Math.PI)]
+        [TestCase(180, System.Math.PI)]
+        [TestCase(360, 2.0 * System.Math.PI)]
+        [TestCase(-90, -0.5 * System.Math.PI)]
+        [TestCase(-180, -System.Math.PI)]
+        [TestCase(-360, -2.0 * System.Math.PI)]
+        public void RadToDeg(double d, double r)
+        {
+            Conversion.RadToDeg(r).ShouldBe(d);
+        }
+    }
+}
