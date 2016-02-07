1c35ab234ca7066149b19796292337c4b0a33fa6
Thierry Matthey <matthey@gmail.com>
2016-02-07 01:06:44 +0100

    Added Vector3D class

---
+++ Src/Vector3D.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+namespace Math
+{
+    public class Vector3D
+    {
+        public Vector3D()
+        {
+        }
+
+        public Vector3D(double c)
+        {
+            X = c;
+            Y = c;
+            Z = c;
+        }
+
+        public Vector3D(Vector3D v)
+        {
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
+        }
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public void Add(Vector3D v)
+        {
+            X += v.X;
+            Y += v.Y;
+            Z += v.Z;
+        }
+
+        public void Set(Vector3D v)
+        {
+            X = v.X;
+            Y = v.Y;
+            Z = v.Z;
+        }
+
+        public void Set(double c)
+        {
+            X = c;
+            Y = c;
+            Z = c;
+        }
+
+        public void Sub(Vector3D v)
+        {
+            X -= v.X;
+            Y -= v.Y;
+            Z -= v.Z;
+        }
+
+        public void Add(double c)
+        {
+            X += c;
+            Y += c;
+            Z += c;
+        }
+
+        public void Sub(double c)
+        {
+            X -= c;
+            Y -= c;
+            Z -= c;
+        }
+
+        public void Mul(double c)
+        {
+            X *= c;
+            Y *= c;
+            Z *= c;
+        }
+
+        public void Div(double c)
+        {
+            X /= c;
+            Y /= c;
+            Z /= c;
+        }
+
+        public double Normalize()
+        {
+            var d = Norm();
+            if (Comparison.IsPositive(d))
+            {
+                Div(d);
+            }
+            return d;
+        }
+
+        public double Norm2()
+        {
+            return Norm2(X, Y, Z);
+        }
+
+        public double Norm()
+        {
+            return Norm(X, Y, Z);
+        }
+
+        public double Distance(Vector3D v)
+        {
+            return Norm(v.X - X, v.Y - Y, v.Z - Z);
+        }
+
+        public double Dot(Vector3D v)
+        {
+            return X * v.X + Y * v.Y + Z * v.Z;
+        }
+
+        public Vector3D Normalized()
+        {
+            var res = new Vector3D(this);
+            res.Normalize();
+            return res;
+        }
+
+        public static Vector3D operator +(Vector3D v1, Vector3D v2)
+        {
+            var res = new Vector3D(v1);
+            res.Add(v2);
+            return res;
+        }
+
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+        {
+            var res = new Vector3D(v1);
+            res.Sub(v2);
+            return res;
+        }
+
+        public static double operator *(Vector3D v1, Vector3D v2)
+        {
+            return v1.Dot(v2);
+        }
+
+        public static Vector3D operator *(Vector3D v, double c)
+        {
+            var res = new Vector3D(v);
+            res.Mul(c);
+            return res;
+        }
+
+        public static Vector3D operator *(double c, Vector3D v)
+        {
+            var res = new Vector3D(v);
+            res.Mul(c);
+            return res;
+        }
+
+        public static Vector3D operator /(Vector3D v, double c)
+        {
+            var res = new Vector3D(v);
+            res.Div(c);
+            return res;
+        }
+
+        static private double Norm2(double x, double y, double z)
+        {
+            return x * x + y * y + z * z;
+        }
+
+        static private double Norm(double x, double y, double z)
+        {
+            return System.Math.Sqrt(Norm2(x, y, z));
+        }
+    }
+}
---
+++ Tests/Vector3DTests.cs
@@ -0,0 +1,376 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+using NUnit.Framework;
+using Shouldly;
+
+namespace Math.Tests
+{
+    [TestFixture]
+    public class Vector3DTests
+    {
+        [Test]
+        public void Vector3D_EmptyConstructor_NullVector()
+        {
+            var v = new Vector3D();
+            v.X.ShouldBe(0);
+            v.Y.ShouldBe(0);
+            v.Z.ShouldBe(0);
+        }
+
+        [TestCase(1.123)]
+        [TestCase(-1.123)]
+        [TestCase(0.0)]
+        public void Vector3D_ConstructorWithConstant_ConstantVector(double c)
+        {
+            var v = new Vector3D(c);
+            v.X.ShouldBe(c);
+            v.Y.ShouldBe(c);
+            v.Z.ShouldBe(c);
+        }
+
+        [Test]
+        public void Vector3D_ConstructorWithVector3D_Vector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var u = new Vector3D();
+            u.X = a;
+            u.Y = b;
+            u.Z = c;
+            var v = new Vector3D(u);
+            u.X.ShouldBe(a);
+            u.Y.ShouldBe(b);
+            u.Z.ShouldBe(c);
+        }
+
+        [Test]
+        public void Vector3D_AddVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            v.Add(u);
+            v.X.ShouldBe(a + d);
+            v.Y.ShouldBe(b + e);
+            v.Z.ShouldBe(c + f);
+        }
+
+        [Test]
+        public void Vector3D_SubVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            v.Sub(u);
+            v.X.ShouldBe(d - a);
+            v.Y.ShouldBe(e - b);
+            v.Z.ShouldBe(f - c);
+        }
+
+        [Test]
+        public void Vector3D_SetVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            v.Set(u);
+            v.X.ShouldBe(a);
+            v.Y.ShouldBe(b);
+            v.Z.ShouldBe(c);
+        }
+
+        [Test]
+        public void Vector3D_SetConstant()
+        {
+            var a = 17.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var v = new Vector3D(d, e, f);
+            v.Set(a);
+            v.X.ShouldBe(a);
+            v.Y.ShouldBe(a);
+            v.Z.ShouldBe(a);
+        }
+
+        [Test]
+        public void Vector3D_AddConstant()
+        {
+            var a = 17.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var v = new Vector3D(d, e, f);
+            v.Add(a);
+            v.X.ShouldBe(d + a);
+            v.Y.ShouldBe(e + a);
+            v.Z.ShouldBe(f + a);
+        }
+
+        [Test]
+        public void Vector3D_SubConstant()
+        {
+            var a = 17.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var v = new Vector3D(d, e, f);
+            v.Sub(a);
+            v.X.ShouldBe(d - a);
+            v.Y.ShouldBe(e - a);
+            v.Z.ShouldBe(f - a);
+        }
+
+        [Test]
+        public void Vector3D_MulConstant()
+        {
+            var a = 17.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var v = new Vector3D(d, e, f);
+            v.Mul(a);
+            v.X.ShouldBe(d * a);
+            v.Y.ShouldBe(e * a);
+            v.Z.ShouldBe(f * a);
+        }
+
+        [Test]
+        public void Vector3D_DivConstant()
+        {
+            var a = 17.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var v = new Vector3D(d, e, f);
+            v.Div(a);
+            v.X.ShouldBe(d / a);
+            v.Y.ShouldBe(e / a);
+            v.Z.ShouldBe(f / a);
+        }
+
+        [Test]
+        public void Vector3D_Norm2()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var v = new Vector3D(a, b, c);
+            v.Norm2().ShouldBe(a * a + b * b + c * c);
+        }
+
+        [Test]
+        public void Vector3D_Norm()
+        {
+            var a = -17.0;
+            var b = -19.0;
+            var c = -23.0;
+            var v = new Vector3D(a, b, c);
+            v.Norm().ShouldBe(System.Math.Sqrt(a * a + b * b + c * c));
+        }
+
+        [Test]
+        public void Vector3D_Distance()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            var x = a - d;
+            var y = b - e;
+            var z = c - f;
+            v.Distance(u).ShouldBe(System.Math.Sqrt(x * x + y * y + z * z));
+        }
+
+        [Test]
+        public void Vector3D_Dot()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            v.Dot(u).ShouldBe(a * d + b * e + c * f);
+        }
+
+        [Test]
+        public void Vector3D_Normalize()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var v = new Vector3D(a, b, c);
+            v.Normalize();
+            v.Norm().ShouldBe(1.0, Comparison.Epsilon);
+        }
+
+        [Test]
+        public void Vector3D_ZeroVector_Normalize()
+        {
+            var v = new Vector3D();
+            v.Normalize();
+            v.Norm().ShouldBe(0.0);
+        }
+
+        [Test]
+        public void Vector3D_Normalized()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var v = new Vector3D(a, b, c);
+            var u = v.Normalized();
+            u.Norm().ShouldBe(1.0, Comparison.Epsilon);
+        }
+
+        [Test]
+        public void Vector3D_ZeroVector_Normalized()
+        {
+            var v = new Vector3D();
+            var u = v.Normalized();
+            u.Norm().ShouldBe(0.0);
+        }
+
+        [Test]
+        public void Vector3D_Vector3DAddOpVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            var w = u + v;
+
+            w.X.ShouldBe(a + d);
+            w.Y.ShouldBe(b + e);
+            w.Z.ShouldBe(c + f);
+        }
+
+        [Test]
+        public void Vector3D_Vector3DSubOpVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            var w = u - v;
+
+            w.X.ShouldBe(a - d);
+            w.Y.ShouldBe(b - e);
+            w.Z.ShouldBe(c - f);
+        }
+
+        [Test]
+        public void Vector3D_Vector3MulOpVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var e = 49.9;
+            var f = -1.1;
+            var u = new Vector3D(a, b, c);
+            var v = new Vector3D(d, e, f);
+            var w = u * v;
+            w.ShouldBe(u.Dot(v));
+        }
+
+        [Test]
+        public void Vector3D_Vector3DMulOpConst()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var u = new Vector3D(a, b, c);
+            var w = u * d;
+            w.X.ShouldBe(a * d);
+            w.Y.ShouldBe(b * d);
+            w.Z.ShouldBe(c * d);
+        }
+
+        [Test]
+        public void Vector3D_ConstMulOpVector3D()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var u = new Vector3D(a, b, c);
+            var w = u * d;
+            w.X.ShouldBe(a * d);
+            w.Y.ShouldBe(b * d);
+            w.Z.ShouldBe(c * d);
+        }
+
+        [Test]
+        public void Vector3D_Vector3DDivOpConst()
+        {
+            var a = 17.0;
+            var b = 19.0;
+            var c = 23.0;
+            var d = 27.05;
+            var u = new Vector3D(a, b, c);
+            var w = u / d;
+            w.X.ShouldBe(a / d);
+            w.Y.ShouldBe(b / d);
+            w.Z.ShouldBe(c / d);
+        }
+    }
+}
