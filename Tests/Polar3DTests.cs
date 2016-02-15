399cf45e059f7b91d8540ffce4f56933caff564a
Thierry Matthey <tmatthey@slb.com>
2016-02-15 20:34:19 +0100

    Added some missing tests.

--- Tests/Polar3DTests.cs
+++ Tests/Polar3DTests.cs
@@ -129,5 +129,63 @@ namespace Math.Tests
             q.Theta.ShouldBe(p.Theta, 1e-13);
             q.Phi.ShouldBe(p.Phi, 1e-13);
         }
+
+        [Test]
+        public void Polar3D_Zero_CasteToVector3D()
+        {
+            var p = new Polar3D(0, 0, 0);
+            var v = (Vector3D) p;
+            v.ShouldBe(Vector3D.Zero);
+        }
+
+        [Test]
+        public void Polar3D_Zero_CasteToPolar3D()
+        {
+            var v = new Vector3D(0, 0, 0);
+            var p = (Polar3D) v;
+            p.ShouldBe(Polar3D.Zero);
+        }
+
+        [Test]
+        public void Polar3D_ConstructorTheataPhi_ReturnsWithRadiusOne()
+        {
+            var p = new Polar3D(0.1, 0.2);
+            p.R.ShouldBe(1.0);
+        }
+
+        [Test]
+        public void Polar3D_EqualInput_Equal_ReturnsTrue()
+        {
+            var p = new Polar3D(0.1, 0.2);
+            var q = new Polar3D(0.1, 0.2 + System.Math.PI*2.0, 1.0);
+            p.R.ShouldBe(1.0);
+            (p == q).ShouldBe(true);
+        }
+
+        [Test]
+        public void Polar3D_NotEqualInput_NotEqual_ReturnsTrue()
+        {
+            var p = new Polar3D(0.1, 0.2);
+            var q = new Polar3D(0.1, 0.2 + System.Math.PI*2.0, 1.1);
+            p.R.ShouldBe(1.0);
+            (p != q).ShouldBe(true);
+        }
+
+        [Test]
+        public void Polar3D_Equals_WithNull_ReturnsFalse()
+        {
+            var p = new Polar3D(0.1, 0.2);
+            Polar3D q = null;
+            p.Equals(q).ShouldBe(false);
+
+        }
+
+        [Test]
+        public void Polar3D_Equalsl_WithItself_ReturnsTrue()
+        {
+            var p = new Polar3D(0.1, 0.2);
+            p.Equals(p).ShouldBe(true);
+
+        }
     }
 }
