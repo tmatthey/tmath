2a8fdfcab68f3a2b6dd9f100ed847a2039d29253
Thierry Matthey <tmatthey@slb.com>
2016-06-13 00:48:29 +0200

    Added DBScan 1st try

---
+++ Src/DBScan.cs
@@ -0,0 +1,153 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using Math.Interfaces;
+using Math.KDTree;
+
+namespace Math
+{
+    public class DBScan<T, S>
+        where T : IArray, IDimension
+        where S : IArray, IDimension, INorm<S>, IBoundingFacade<T>
+    {
+        private readonly IList<S> _list;
+        private IList<Point> _data;
+        private ITree<T> _tree;
+
+        public DBScan(IList<S> list)
+        {
+            _list = list;
+            _tree = null;
+            _data = null;
+        }
+
+        public IEnumerable<IList<int>> Cluster(double eps = 25.0, int n = 5)
+        {
+            // Corner cases
+            if (_list.Count < n)
+                return new List<List<int>>();
+
+            // Intialize
+            if (_tree == null)
+                _tree = TreeBuilder.Build<T, S>(_list);
+            if (_data == null)
+            {
+                _data = new List<Point>();
+                foreach (var t in _list)
+                    _data.Add(new Point(t));
+            }
+            else
+            {
+                foreach (var t in _data)
+                {
+                    t.Visited = false;
+                    t.ClusterId = Classification.Unclassified;
+                }
+            }
+
+            // Search
+            var clusterId = 0;
+            foreach (var p in _data)
+            {
+                if (p.Visited)
+                    continue;
+
+                p.Visited = true;
+                var allNeighbors = EpsNeighborhood(p, eps).ToList();
+                if (allNeighbors.Count < n)
+                {
+                    p.ClusterId = Classification.Noise;
+                    continue;
+                }
+
+                p.ClusterId = clusterId;
+                for (var j = 0; j < allNeighbors.Count; j++)
+                {
+                    var pn = _data[allNeighbors[j]];
+                    if (!pn.Visited)
+                    {
+                        pn.Visited = true;
+                        var neighbors = EpsNeighborhood(pn, eps).ToList();
+                        if (neighbors.Count() >= n)
+                            allNeighbors = allNeighbors.Union(neighbors).ToList();
+                    }
+                    if (pn.ClusterId == Classification.Unclassified)
+                        pn.ClusterId = clusterId;
+                }
+                clusterId++;
+            }
+
+            // Aggregate result
+            var clusters = new List<List<int>>();
+            for (var i = 0; i < clusterId; i++)
+                clusters.Add(new List<int>());
+
+            for (var i = 0; i < _data.Count; i++)
+            {
+                var j = _data[i].ClusterId;
+                if (j >= 0)
+                    clusters[j].Add(i);
+            }
+
+            return clusters;
+        }
+
+        private IEnumerable<int> EpsNeighborhood(Point seed, double eps)
+        {
+            var bounding = seed.Value.Bounding();
+            bounding.ExpandLayer(eps);
+            var inside = _tree.Search(bounding.Min, bounding.Max).Distinct();
+
+            return inside.Where(
+                index =>
+                    _data[index] != seed && Comparison.IsLessEqual(seed.Value.ModifiedNorm(_data[index].Value), eps));
+        }
+
+        private class Point
+        {
+            public readonly S Value;
+            public int ClusterId;
+            public bool Visited;
+
+            public Point(S value)
+            {
+                Value = value;
+                Visited = false;
+                ClusterId = Classification.Unclassified;
+            }
+        }
+
+        private static class Classification
+        {
+            public const int Unclassified = -2;
+            public const int Noise = -1;
+        };
+    }
+}
---
+++ Tests/DBScanTests.cs
@@ -0,0 +1,261 @@
+﻿/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MIT
+ *
+ * Copyright (c) 2016 Thierry Matthey
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ***** END LICENSE BLOCK *****
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+using Math.Gfx;
+using Math.Gps;
+using Math.Tests.Gps;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Math.Tests
+{
+    [TestFixture]
+    public class DBScanTests
+    {
+        private readonly GpsTrackExamples _gpsTrackExamples = new GpsTrackExamples();
+        public class Segment2DExt : Segment2D
+        {
+            public Segment2DExt(Vector2D a, Vector2D b, int i0, int i1, int j, int k)
+            {
+                A = a;
+                B = b;
+                IA = i0;
+                IB = i1;
+                J = j;
+                K = k;
+                U = a;
+                V = b;
+            }
+
+            public int IA { get; set; }
+            public int IB { get; set; }
+            public int J { get; set; }
+            public int K { get; set; }
+            public Vector2D U { get; set; }
+            public Vector2D V { get; set; }
+        }
+
+        public class Vector2DExt : Vector2D
+        {
+            public Vector2DExt(Vector2D v, int i, int j, int k)
+                : base(v)
+            {
+                I = i;
+                J = j;
+                K = k;
+            }
+
+            public int I { get; set; }
+            public int J { get; set; }
+            public int K { get; set; }
+        }
+
+        [Test]
+        public void DBScan_EmptyList_ReturnsEmpty()
+        {
+            var list = new List<Segment2D> {new Segment2D(Vector2D.E1, Vector2D.E2)};
+            var dbs = new DBScan<Vector2D, Segment2D>(list);
+            var res = dbs.Cluster().ToList();
+            res.Count.ShouldBe(0);
+        }
+
+        [Test]
+        public void DBScan_OneElementList_ReturnsZero()
+        {
+            //var tracks = TrajectoryExamples.deer_1995();
+            //tracks.Count.ShouldBe(32);
+            //var box = new BoundingRect();
+            //foreach (var p in tracks.SelectMany(track => track))
+            //    box.Expand(p);
+            var gpsTracks = new List<GpsTrack>
+            {
+                new GpsTrack(_gpsTrackExamples.TrackOne()),
+                new GpsTrack(_gpsTrackExamples.TrackTwo()),
+                new GpsTrack(_gpsTrackExamples.TrackThree()),
+                new GpsTrack(_gpsTrackExamples.TrackFour()),
+                new GpsTrack(_gpsTrackExamples.TrackFive())
+            };
+            var center = HeatMap.CalculateCenter(gpsTracks, null);
+            var tracks = new List<List<Vector2D>>();
+            var box = new BoundingRect();
+            foreach (var track in gpsTracks)
+            {
+                var transformed = track.CreateTransformedTrack(center);
+                tracks.Add(transformed.Track);
+                box.Expand(transformed.Size);
+            }
+
+
+            var n = 2;
+            var eps = 50;
+            var minL = 50.0;
+
+            var segments = new List<Segment2D>();
+            var k = 0;
+            foreach (var track in tracks)
+            {
+                var significantPoints = Geometry.SignificantPoints(track);
+                var segs =
+                    Geometry.PolylineToSegmentPointList(significantPoints.Select(i => track[i]).ToList(), minL).ToList();
+                for (var j = 0; j < segs.Count; j += 2)
+                {
+                    var i0 = significantPoints[segs[j]];
+                    var i1 = significantPoints[segs[j + 1]];
+                    var s = new Segment2DExt(track[i0], track[i1], i0, i1, j/2, k);
+                    segments.Add(s);
+                }
+                k++;
+            }
+            //segments.Count.ShouldBe(1879);
+
+            var dbs = new DBScan<Vector2D, Segment2D>(segments);
+            var clusterList = dbs.Cluster(eps, n).ToList();
+            //clusterList.Count.ShouldBe(17);
+
+            var clusterPointList = new List<List<Vector2D>>();
+            var clusters = clusterList.Select(cluster => cluster.Select(s => (Segment2DExt) (segments[s])).ToList()).ToList();
+            foreach (var cluster in clusters)
+            {
+                var trajectories = new HashSet<int>();
+                foreach (var s in cluster)
+                    trajectories.Add(s.K);
+                if (trajectories.Count < n)
+                    continue;
+
+                var avg = new Vector2D();
+                avg = cluster.Aggregate(avg, (c, s) => c + s.Vector())/cluster.Count;
+                var angle = -Vector2D.E1.Angle(avg);
+                var points = new List<Vector2DExt>();
+                var j = 0;
+                foreach (var s in cluster)
+                {
+                    s.U = s.A.Rotate(angle);
+                    s.V = s.B.Rotate(angle);
+                    points.Add(new Vector2DExt(s.U, s.IA, j, s.K));
+                    points.Add(new Vector2DExt(s.V, s.IB, j, s.K));
+                    j++;
+                }
+                points = points.OrderBy(p => p.X).ToList();
+
+                var lineSegments = new HashSet<int>();
+                var current = points.First();
+                var next = points.First();
+                var prevValue = points.First().X;
+                var clusterPoints = new List<Vector2D>();
+
+                for (var i = 0; i < points.Count;)
+                {
+                    var del = new HashSet<int>();
+                    var insert = new HashSet<int>();
+                    do
+                    {
+                        current = points[i];
+                        i++;
+                        if (!lineSegments.Contains(current.J))
+                        {
+                            insert.Add(current.J);
+                            lineSegments.Add(current.J);
+                        }
+                        else
+                        {
+                            del.Add(current.J);
+                        }
+
+                        if (i + 1 < points.Count)
+                        {
+                            next = points[i];
+                            continue;
+                        }
+                        break;
+                    } while (Comparison.IsEqual(current.X, next.X));
+
+                    foreach (var i0 in insert)
+                    {
+                        foreach (var i1 in del)
+                        {
+                            if (i0 == i1)
+                            {
+                                lineSegments.Remove(i1);
+                                del.Remove(i1);
+                                break;
+                            }
+                        }
+                        foreach (var i1 in del)
+                        {
+                            if (cluster[i1].K == cluster[i0].K)
+                            {
+                                lineSegments.Remove(i1);
+                                del.Remove(i1);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (lineSegments.Count >= n &&
+                        Comparison.IsLessEqual(minL/1.414, System.Math.Abs(current.X - prevValue)))
+                    {
+                        var sum = new Vector2D();
+
+                        foreach (var seg in lineSegments)
+                        {
+                            var s = cluster[seg];
+                            var c = (current.X - s.U.X) / (s.V.X - s.U.X);
+                            var y = s.U.Y + c * (s.V.Y - s.U.Y);
+                            sum += new Vector2D(current.X, y);
+                        }
+                        prevValue = current.X;
+                        clusterPoints.Add((sum / lineSegments.Count).Rotate(-angle));
+                    }
+
+                    foreach (var i1 in del)
+                        lineSegments.Remove(i1);
+                }
+                if (clusterPoints.Any())
+                    clusterPointList.Add(clusterPoints);
+            }
+            var bitmap = new Bitmap(box.Min-Vector2D.One, box.Max+Vector2D.One, 1.0);
+            foreach (var list in clusterPointList)
+            {
+                for (var i = 0; i + 1 < list.Count; i++)
+                    Draw.XiaolinWu(list[i], list[i + 1], bitmap.Add);
+            }
+            /*
+            foreach (var list in tracks)
+            {
+                for (var i = 0; i + 1 < list.Count; i++)
+                    Draw.XiaolinWu(list[i], list[i + 1], bitmap.Add);
+            }*/
+            BitmapFileWriter.PNG("cluster.png", bitmap.Pixels);
+
+
+        }
+    }
+}
