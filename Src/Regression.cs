9d98a556860ac9a58880d28ca9be9906ddf4bd85
Thierry Matthey <tmatthey@slb.com>
2016-09-13 23:56:12 +0200

    Added second order regression.

--- Src/Regression.cs
+++ Src/Regression.cs
@@ -73,5 +73,58 @@ namespace Math
                 }
             }
         }
+
+        // Second order regression without weight : a*x^2 + b*x + c
+        public static void Quadratic(IList<double> x, IList<double> y, out double a, out double b, out double c)
+        {
+            IList<double> w = Enumerable.Repeat(1.0, x.Count).ToList();
+            Quadratic(x, y, w, out a, out b, out c);
+        }
+
+        // Second order regression weighted : a*x^2 + b*x + c
+        public static void Quadratic(IList<double> x, IList<double> y, IList<double> w, out double a, out double b,
+            out double c)
+        {
+            a = double.NaN;
+            b = double.NaN;
+            c = double.NaN;
+            if (x.Count > 0 && x.Count == y.Count && y.Count == w.Count)
+            {
+                var w1 = 0.0;
+                var wx = 0.0;
+                var wx2 = 0.0;
+                var wx3 = 0.0;
+                var wx4 = 0.0;
+                var wy = 0.0;
+                var wyx = 0.0;
+                var wyx2 = 0.0;
+
+                for (var i = 0; i < x.Count; ++i)
+                {
+                    w1 += w[i];
+                    var tmpx = w[i]*x[i];
+                    wx += tmpx;
+                    tmpx *= x[i];
+                    wx2 += tmpx;
+                    tmpx *= x[i];
+                    wx3 += tmpx;
+                    tmpx *= x[i];
+                    wx4 += tmpx;
+                    var tmpy = w[i]*y[i];
+                    wy += tmpy;
+                    tmpy *= x[i];
+                    wyx += tmpy;
+                    tmpy *= x[i];
+                    wyx2 += tmpy;
+                }
+
+                var den = wx2*wx2*wx2 - 2.0*wx3*wx2*wx + wx4*wx*wx + wx3*wx3*w1 - wx4*wx2*w1;
+                if (Comparison.IsEqual(den, 0.0))
+                    return;
+                a = (wx*wx*wyx2 - wx2*w1*wyx2 - wx2*wx*wyx + wx3*w1*wyx + wx2*wx2*wy - wx3*wx*wy)/den;
+                b = (-wx2*wx*wyx2 + wx3*w1*wyx2 + wx2*wx2*wyx - wx4*w1*wyx - wx3*wx2*wy + wx4*wx*wy)/den;
+                c = (wx2*wx2*wyx2 - wx3*wx*wyx2 - wx3*wx2*wyx + wx4*wx*wyx + wx3*wx3*wy - wx4*wx2*wy)/den;
+            }
+        }
     }
 }
