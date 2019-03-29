4a6ba62d2afcfc5a20a484e49dd83a39a81504a2
Thierry Matthey <matthey@gmail.com>
2019-03-29 21:51:15 +0100

    Update copyright year

--- Math/Gfx/HeatMap.cs
+++ Math/Gfx/HeatMap.cs
@@ -2,7 +2,7 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Version: MIT
  *
- * Copyright (c) 2016-2018 Thierry Matthey
+ * Copyright (c) 2016-2019 Thierry Matthey
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
